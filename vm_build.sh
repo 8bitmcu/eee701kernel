9a2d40b0a3cd700a9d749f30ffb37e52faee319b
agashka <agashka2@gmail.com>
2016-07-17 04:18:32 -0300

    Update vm_build.sh

--- vm_build.sh
+++ vm_build.sh
@@ -1,30 +1,68 @@
 #!/bin/bash
 
-echo "building kernel $KERNEL, version $VERSION"
+usage() {
+    cat <<EOF
+Usage: $CMD <subcommand> [options] [args]
 
-# download or copy kernel if exists on host
-curl -O https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/$KERNEL.tar.xz
+subcommands:
+  kernel [options]
+    -c|--clean                clean
+  image [options]
+    -c|--clean                clean
+  help                      this help
+EOF
+}
 
-# extract the kernel, change to dir
-tar -xJf $KERNEL.tar.xz
-cd $KERNEL
+kernel() {
+  echo "building kernel $KERNEL, version $VERSION"
+  
+  # download or copy kernel if exists on host
+  curl -O https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/$KERNEL.tar.xz
+  
+  # extract the kernel, change to dir
+  tar -xJf $KERNEL.tar.xz
+  cd $KERNEL
+  
+  # clean
+  make-kpkg clean
+  
+  ### copy kernel config file
+  cp /host/.config /$KERNEL/
+  
+  # compile kernel & package
+  make-kpkg kernel_image
+}
 
-# clean
-make-kpkg clean
+image() {
+  # debirf
+  rm -rf ../debirf
+  mkdir ../debirf
+  cd ../debirf
+  cp /usr/share/doc/debirf/example-profiles/minimal.tgz minimal.tgz
+  tar -xf minimal.tgz
+  
+  # make debirf
+  debirf make -k ../linux-image-$VERSION_i386.deb minimal
+  debird makeiso minimal
+}
 
-### copy kernel config file
-cp /host/.config /$KERNEL/
 
-# compile kernel & package
-make-kpkg kernel_image
+COMMAND="$1"
+[ "$COMMAND" ] || failure "Type '$CMD help' for usage."
+shift
 
-# debirf
-rm -rf ../debirf
-mkdir ../debirf
-cd ../debirf
-cp /usr/share/doc/debirf/example-profiles/minimal.tgz minimal.tgz
-tar -xf minimal.tgz
-
-# make debirf
-debirf make -k ../linux-image-$VERSION_i386.deb minimal
-debird makeiso minimal
+case $COMMAND in
+    'kernel'|'k')
+        kernel "$@"
+        ;;
+    'image'|'i')
+        image "$@"
+        ;;
+    'help'|'h'|'?'|'-h'|'--help')
+        usage
+        ;;
+    *)
+        failure "Unknown command: '$COMMAND'
+Type '$CMD help' for usage."
+        ;;
+esac
